66087a29ecac5bd169f709c8e4738aad6ea28e5d
Leon Cai <leoncaiau912@gmail.com>
2018-09-17 00:49:45 +0000

    Seperate docker installation from playbook

--- provision.sh
+++ provision.sh
@@ -8,6 +8,10 @@ set -e
  sudo pip install ansible
  sudo timedatectl set-timezone Europe/Istanbul
  sudo localectl set-locale LANG=en_US.utf8
+ sudo wget 'https://raw.githubusercontent.com/leoncaiau912/packer/master/docker-install.sh'
+ echo "Running docker installation."
+ sudo bash docker-install.sh
+ 
  sudo wget 'https://raw.githubusercontent.com/leoncaiau912/packer/master/i_playbook.yml'
  echo "Running build."
  sudo ansible-playbook i_playbook.yml
