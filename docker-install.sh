b2e7cf755c48f658c8e42c16a7a17882f30177d9
Leon Cai <leoncaiau912@gmail.com>
2019-08-12 21:46:49 -0400

    Fixed apt install force for apt-transport-https

--- docker-install.sh
+++ docker-install.sh
@@ -3,7 +3,7 @@ set -e
 sudo true
 
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates
+sudo apt-get install -y apt-transport-https ca-certificates 
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list'
 sudo apt-get update
