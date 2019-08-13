c69c34772e581e8e56fe5003c8d8018fa870ddec
Leon Cai <leoncaiau912@gmail.com>
2019-08-13 01:01:49 -0400

    Fxied ubuntu 18-04 with template

--- docker-install.sh
+++ docker-install.sh
@@ -6,8 +6,8 @@ sudo apt-get update
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable" 
-sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get update -y
+sudo apt-get install -y docker-ce
 
 sudo apt-get install -y python-pip
 sudo service docker start
