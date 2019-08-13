f14b6bf1bad552d9c2b6edb653a6fe587319fc4b
Leon Cai <leoncaiau912@gmail.com>
2019-08-13 01:23:15 -0400

    Fixed apt get force installation

--- provision.sh
+++ provision.sh
@@ -4,7 +4,7 @@ set -e
  sudo apt-get update
  echo "apt-get update done."
  sudo apt-get -y upgrade
- sudo apt-get install -y python python-pip
+ sudo apt-get install  python python-pip -y
  sudo pip install ansible
 #issue fix
 #https://github.com/pypa/pip/issues/5447
